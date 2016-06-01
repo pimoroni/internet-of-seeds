b4e28538daaf75d74503ea76d88b99830fc21bcf
sandyjmacdonald <sandyjmacdonald@gmail.com>
2016-06-01 16:29:14 +0100

    Added experimental IR imaging and IR config options.

--- internet-of-seeds-tweet.py
+++ internet-of-seeds-tweet.py
@@ -1,12 +1,14 @@
 import json
 import tweepy
+import datetime
+
+with open(config_f) as f:
+  config = json.load(f)
 
 ## Function to tweet sensor values and a timestamped image. Needs to be passed
 ## status (timestamp and sensor values pulled from data log) and the latest
 ## image filename.
-def tweet_pic(status, latest, config_f='config.json'):
-  with open(config_f) as f:
-    config = json.load(f)
+def tweet_pic(status, latest, config=config):
   ckey = config['tweepy']['ckey']
   csecret = config['tweepy']['csecret']
   akey = config['tweepy']['akey']
@@ -17,16 +19,20 @@ def tweet_pic(status, latest, config_f='config.json'):
   api.update_with_media(latest, status=status)
 
 ## Set the latest image filename, grab the last line from the data log.
+if config['ir'] == 1:
+  latest = 'data/latest_ir_ts.jpg'
+  t = datetime.datetime.now()
+  ts = t.strftime('%Y-%m-%d-%H-%M')
+  status = ts + '.'
+else:
+  latest = 'data/latest_ts.jpg'
+  fn = 'data/internet-of-seeds.log'
 
-latest = 'data/latest_ts.jpg'
+  with open(fn) as f:
+    for l in f.readlines():
+      pass
 
-fn = 'data/internet-of-seeds.log'
-with open(fn) as f:
-  for l in f.readlines():
-    pass
+  sensor_vals = l.rstrip().split('\t')
+  status = '%s: Temp: %s C, Press: %s hPa, Light: %s lux, RGB: %s,%s,%s.' % tuple(sensor_vals)
 
-## Format the sensor values nicely for tweeting, run the tweet_pic function.
-
-sensor_vals = l.rstrip().split('\t')
-status = '%s: Temp: %s C, Press: %s hPa, Light: %s lux, RGB: %s,%s,%s.' % tuple(sensor_vals)
 tweet_pic(status, latest)
--- internet-of-seeds.py
+++ internet-of-seeds.py
@@ -1,47 +1,87 @@
 import os
 import shutil
+import json
 import datetime
 import picamera
 import pandas as pd
+import numpy as np
 import PIL
 from PIL import Image, ImageFont, ImageDraw
 import flotilla
 from sparkblocks import spark
 
+with open(config_f) as f:
+  config = json.load(f)
+
 ## Captures an image and copies to latest.jpg. Needs to be passed a datetime
 ## object for the timestamped image, t.
-def capture_image(t):
+def capture_image(t, config=config):
   ts = t.strftime('%Y-%m-%d-%H-%M')
   cam = picamera.PiCamera()
   cam.resolution = (3280, 2464)
   cam.hflip = True
   cam.vflip = True
-  filename = 'data/image-' + t.strftime('%Y-%m-%d-%H-%M') + '.jpg'
-  cam.capture(filename, quality=100)
-  shutil.copy2(filename, 'data/latest.jpg')
+  if config['ir'] == 1:
+    filename = 'data/image-' + t.strftime('%Y-%m-%d-%H-%M') + '_ir.jpg'
+    cam.capture(filename, quality=100)
+    shutil.copy2(filename, 'data/latest_ir.jpg')
+  else:
+    filename = 'data/image-' + t.strftime('%Y-%m-%d-%H-%M') + '.jpg'
+    cam.capture(filename, quality=100)
+    shutil.copy2(filename, 'data/latest.jpg')
   return filename
+
+## Captures an NDVI image from the Raspberry Pi NoIR camera that shows
+## green vegetative growth in the plants.
+def ndvi_process(img):
+  img = img.convert('RGB')
+  data = np.array(img).astype('float64')
+  num = data[:,:,0] - data[:,:,1]
+  denom = data[:,:,0] + data[:,:,1]
+  num += 0.01
+  denom += 0.01
+  data[:,:,1] = (num / denom)
+  low_vals = data < 128
+  data[low_vals] = 0
+  g = data[:,:,0].copy()
+  r = data[:,:,1].copy()
+  data[:,:,0] = r
+  data[:,:,1] = g
+  data = np.uint8(data)
+  ndvi_img = Image.fromarray(data)
+  ndvi_img.save(img)
+  return True
 
 ## Overlays the timestamp and sensor values on the latest captured image. Needs
 ## to be passed a datetime object and the dictionary of sensor values.
-def timestamp_image(t, sensor_vals, sparks, watermark=False):
+def timestamp_image(t, sensor_vals, sparks, watermark=False, config=config):
   ts_read = t.strftime('%H:%M, %a. %d %b %Y')
-  img = Image.open('data/latest.jpg')
+  if config['ir'] == 1:
+    img = Image.open('data/latest_ir.jpg')
+  else:
+    img = Image.open('data/latest.jpg')
   img = img.resize((1438, 1080))
   if watermark == True:
     wm = Image.open('data/watermark.png')
-    img.paste(wm, (0, 996), wm)
+  img.paste(wm, (0, 996), wm)
   draw = ImageDraw.Draw(img)
   font = ImageFont.truetype('data/Roboto-Regular.ttf', 36)
-  spark_font = ImageFont.truetype('data/arial-unicode-ms.ttf', 16)
-  draw.text((10, 10), ts_read, (255, 255, 255), font=font)
-  draw.text((10, 50), 'Temp: {0:.2f}'.format(sensor_vals['temperature']), (255, 255, 255), font=font)
-  draw.text((10, 90), 'Press: {0:.2f}'.format(sensor_vals['pressure']), (255, 255, 255), font=font)
-  draw.text((10, 130), 'Light: {0:.2f}'.format(sensor_vals['light']), (255, 255, 255), font=font)
-  draw.text((10, 170), 'RGB: ' + ','.join([str(int(i)) for i in sensor_vals['colour']]), (255, 255, 255), font=font)
-  for i in range(len(sparks)):
-    draw.text((10, 225 + i * 25), sparks[i], (255, 255, 255), font=spark_font)
-  filename = 'data/latest_ts.jpg'
-  img.save(filename)
+  if config['ir'] == 1:
+    ndvi_process(img)
+    draw.text((10, 10), ts_read, (255, 255, 255), font=font)
+    filename = 'data/latest_ir_ts.jpg'
+    img.save(filename)
+  else:
+    spark_font = ImageFont.truetype('data/arial-unicode-ms.ttf', 16)
+    draw.text((10, 10), ts_read, (255, 255, 255), font=font)
+    draw.text((10, 50), 'Temp: {0:.2f}'.format(sensor_vals['temperature']), (255, 255, 255), font=font)
+    draw.text((10, 90), 'Press: {0:.2f}'.format(sensor_vals['pressure']), (255, 255, 255), font=font)
+    draw.text((10, 130), 'Light: {0:.2f}'.format(sensor_vals['light']), (255, 255, 255), font=font)
+    draw.text((10, 170), 'RGB: ' + ','.join([str(int(i)) for i in sensor_vals['colour']]), (255, 255, 255), font=font)
+    for i in range(len(sparks)):
+      draw.text((10, 225 + i * 25), sparks[i], (255, 255, 255), font=spark_font)
+    filename = 'data/latest_ts.jpg'
+    img.save(filename)
   return filename
 
 ## Reads the Flotilla sensor values and stores them in a dictionary.
@@ -101,8 +141,13 @@ def log_values(t, sensor_vals):
 
 t = datetime.datetime.now()
 img = capture_image(t)
-sensor_vals = read_sensors()
-data = 'data/internet-of-seeds.log'
-sparks = sparklines(data)
-latest = timestamp_image(t, sensor_vals, sparks, watermark=True)
-log_values(t, sensor_vals)
+if config['ir'] == 1:
+  sensor_vals = None
+  sparks = None
+  latest = timestamp_image(t, sensor_vals, sparks, watermark=True)
+else:
+  sensor_vals = read_sensors()
+  data = 'data/internet-of-seeds.log'
+  sparks = sparklines(data)
+  latest = timestamp_image(t, sensor_vals, sparks, watermark=True)
+  log_values(t, sensor_vals)
